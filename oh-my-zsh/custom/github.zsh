3294d3ac9bad30a2fdebdd27725adcfab57c4ab5
Manolis Tsagias <manolis@tsagias.com>
2018-01-02 12:28:15 +0200

    remove github.zsh

--- oh-my-zsh/custom/github.zsh
+++
@@ -1,9 +0,0 @@
-# git shortcut
-if which hub &> /dev/null; then
-  alias git=hub;
-fi
-alias gtree='g showtree'
-alias glogm='glog --pretty=medium'
-alias glogf='glog --pretty=fuller'
-# github api token
-export HOMEBREW_GITHUB_API_TOKEN=4809d5d904d4d9df3a4e9db53f3787d19f83255c
